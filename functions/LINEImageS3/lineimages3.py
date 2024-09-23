fc8bfebb22ed4ad054dcc5410b80277000e7e5e8
kusaka-dev <kusaka.aws@gmail.com>
2024-09-23 14:28:02 +0000

    S3 putできなかったので修正

--- functions/LINEImageS3/lineimages3.py
+++ functions/LINEImageS3/lineimages3.py
@@ -5,9 +5,9 @@ import boto3
 from linebot import LineBotApi, WebhookHandler
 from linebot.models import TextSendMessage
 
-s3 = boto3.resource('s3')
+s3_client = boto3.client('s3')
 # 環境変数を取得
-s3_backet_name = os.getenv('S3_BUCKET_NAME')
+s3_bucket_name = os.getenv('S3_BUCKET_NAME')
 channel_secret = os.getenv('LINE_CHANNEL_SECRET')
 line_bot_api = LineBotApi(os.getenv('LINE_CHANNEL_ACCESS_TOKEN'))
 
@@ -20,23 +20,13 @@ def lambda_handler(event, context):
     replytoken = event["replytoken"]
     user_id = event["user_id"]
     image = line_bot_api.get_message_content(imageid)
-
-    with tempfile.TemporaryFile() as tmp:
-        for chunk in image.iter_content():
-            tmp.write(chunk)
-        tmp.seek(0)
-    
-    bucket = s3.Bucket(s3_backet_name)
-    bucket.put_object(
-            Body=tmp,
-            Key=f'{imageid}.png'
-        )
+    s3_client.put_object(Bucket=s3_bucket_name, Key=f'{imageid}.png', Body=image_content.image)
     line_bot_api.reply_message(
             replytoken,
             TextSendMessage(text='アップロードに成功しました')
         )
     return {
-        "bucketname": s3_backet_name,
+        "bucketname": s3_bucket_name,
         "imagename": str(imageid) + ".png",
         "user_id": user_id
     }

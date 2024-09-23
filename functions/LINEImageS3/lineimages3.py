f8f907733673b8923dbe274bebd32b41d250e314
kusaka-dev <kusaka.aws@gmail.com>
2024-09-23 13:46:05 +0000

    S3へのアップロードロジックを追加

--- functions/LINEImageS3/lineimages3.py
+++ functions/LINEImageS3/lineimages3.py
@@ -7,7 +7,7 @@ from linebot.models import TextSendMessage
 
 s3 = boto3.resource('s3')
 # 環境変数を取得
-backet_name = os.getenv('BUCKET_NAME')
+s3_backet_name = os.getenv('S3_BUCKET_NAME')
 channel_secret = os.getenv('LINE_CHANNEL_SECRET')
 line_bot_api = LineBotApi(os.getenv('LINE_CHANNEL_ACCESS_TOKEN'))
 
@@ -21,22 +21,22 @@ def lambda_handler(event, context):
     user_id = event["user_id"]
     image = line_bot_api.get_message_content(imageid)
 
-    # with tempfile.TemporaryFile() as tmp:
-    #     for chunk in image.iter_content():
-    #         tmp.write(chunk)
-    #     tmp.seek(0)
-    #
-    # bucket = s3.Bucket(backet_name)
-    # bucket.put_object(
-    #         Body=tmp,
-    #         Key=f'{imageid}.png'
-    #     )
+    with tempfile.TemporaryFile() as tmp:
+        for chunk in image.iter_content():
+            tmp.write(chunk)
+        tmp.seek(0)
+    
+    bucket = s3.Bucket(s3_backet_name)
+    bucket.put_object(
+            Body=tmp,
+            Key=f'{imageid}.png'
+        )
     line_bot_api.reply_message(
             replytoken,
             TextSendMessage(text='アップロードに成功しました')
         )
     return {
-        "bucketname": backet_name,
+        "bucketname": s3_backet_name,
         "imagename": str(imageid) + ".png",
         "user_id": user_id
     }

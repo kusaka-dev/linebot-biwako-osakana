405d88d066120247f7cfcb7926a9da9403ea4854
kusaka <kusaka@kusakanoMacBook-Air.local>
2024-09-23 12:09:31 +0900

    messaging api連携

---
+++ functions/CallSFN/callsfn.py
@@ -0,0 +1,51 @@
+import json
+import os
+from types import BuiltinMethodType
+import datetime
+import base64
+import hashlib
+import hmac
+import boto3
+from linebot import LineBotApi, WebhookHandler
+from linebot.models import TextSendMessage
+
+client = boto3.client('stepfunctions')
+
+# 環境変数を取得
+handler = WebhookHandler(os.getenv('LINE_CHANNEL_SECRET'))
+line_bot_api = LineBotApi(os.getenv('LINE_CHANNEL_ACCESS_TOKEN'))
+statemachine_arn = os.getenv('STATEMACHINE_ARN')
+
+def lambda_handler(event, context):
+    body = event["body"]
+    signature = event["headers"]["x-line-signature"]
+    hash = hmac.new(channel_secret.encode('utf-8'),
+                    body.encode('utf-8'), hashlib.sha256).digest()
+    if signature != base64.b64encode(hash).decode():
+        return {
+            "statusCode": 401,
+            "body": 'Unauthorized'
+        }
+
+    now = datetime.datetime.now().strftime("%m-%d-%Y-%H-%M-%S")
+    jsonbody = json.loads(body)
+    imageid = jsonbody["events"][0]["message"]["id"]
+    replytoken = jsonbody["events"][0]["replyToken"]
+    user_id = jsonbody["events"][0]["source"]["userId"]
+    type = jsonbody["events"][0]["message"]["type"]
+    if type == "image":
+        print('画像が送られました。')
+        # client.start_execution(
+        #     stateMachineArn=statemachine_arn,
+        #     name="job-" + str(now),
+        #     input=json.dumps({
+        #         "imageid": imageid,
+        #         "replytoken": replytoken,
+        #         "user_id": user_id
+        #     })
+        # )
+    # きたメッセージが画像以外の場合、ステートマシンは実行せず、「判定したい魚の写真を送ってください」と返信して終了。
+    else:
+        line_bot_api.reply_message(replytoken, TextSendMessage(
+            text='判定したい魚の写真を送ってください'))
+    return {'body': 'ok'}

c3abf851d943278c01a8ac3991d6b3a503f0a8a5
kusaka-dev <kusaka.aws@gmail.com>
2024-09-25 21:56:45 +0000

    rekognitiondb テスト

---
+++ functions/RekognitionDB/rekognitiondb.py
@@ -0,0 +1,22 @@
+import os
+import boto3
+from linebot import LineBotApi, WebhookHandler
+from linebot.models import TextSendMessage
+
+# 環境変数を取得
+channel_secret = os.getenv('LINE_CHANNEL_SECRET')
+line_bot_api = LineBotApi(os.getenv('LINE_CHANNEL_ACCESS_TOKEN'))
+
+def lambda_handler(event, context):
+    imageid = event["imageid"]
+    replytoken = event["replytoken"]
+    user_id = event["user_id"]
+    line_bot_api.reply_message(
+            user_id,
+            TextSendMessage(imagename)
+        )
+    return {
+        "bucketname": s3_bucket_name,
+        "imagename": str(imageid) + ".png",
+        "user_id": user_id
+    }

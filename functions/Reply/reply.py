4d9e967e528c7f5862ccc57bb6a7bd033d99cfb8
kusaka-dev <kusaka.aws@gmail.com>
2024-10-05 01:16:17 +0000

    modify reply.py

--- functions/Reply/reply.py
+++ functions/Reply/reply.py
@@ -8,6 +8,4 @@ channel_secret = os.getenv('LINE_CHANNEL_SECRET')
 line_bot_api = LineBotApi(os.getenv('LINE_CHANNEL_ACCESS_TOKEN'))
 
 def lambda_handler(event, context):
-    replytoken = event["replytoken"]
-    print(replytoken)
     line_bot_api.push_message(event["user_id"],TextSendMessage(text=event["message"]))
